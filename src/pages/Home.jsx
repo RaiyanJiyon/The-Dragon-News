772a77be148c7c69087eb0432d38e60abd90960c
RaiyanJiyon <raiyanjiyon814@gmail.com>
2024-11-15 07:46:36 +0600

    feat(home): Added Marquee section
    Implement the React Fast Marquee section to display scrolling match highlights.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,18 @@
 import Header from "../components/shared/Header"
+import Marquee from "react-fast-marquee";
 
 const Home = () => {
     return (
-        <div>
+        <div className="w-11/12 mx-auto">
             <Header />
+
+            {/* React Fast Marquee section */}
+            <div className="flex items-center gap-4 bg-[#F3F3F3] p-4 mt-8">
+                <button className="btn bg-[#D72050] text-white font-medium">Latest</button>
+                <Marquee pauseOnHover speed={100}>
+                    Match Highlights: Germany vs Spain — as it happened   !   Match Highlights: Germany vs Spain as...
+                </Marquee>
+            </div>
         </div>
     );
 };
